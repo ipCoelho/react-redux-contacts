7955f19178417ab039ccb51f8127930b1cc23c75
ipCoelho <ipcoelho.business@gmail.com>
2022-08-22 07:02:32 -0300

    wip: initial files added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(props) {
+  return (
+    <div className='ui fixed menu'>
+      <div className='ui container center'>
+        <h1>Contact Manager</h1>
+      </div>
+    </div>
+  );
+}
